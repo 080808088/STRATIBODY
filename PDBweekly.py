ab9b29314ca4d3de55a666acf6f648e39df9c549
14802292 <14802292@hpc01.fpg.local>
2021-07-29 11:56:45 +0200

    improved

--- PDBweekly.py
+++ PDBweekly.py
@@ -2,41 +2,32 @@
 
 from rcsbsearch import TextQuery
 from rcsbsearch import rcsb_attributes as attrs
+import webbrowser
+
+with open('antibody.txt', 'w') as f:
 
 # #costruisco le query (attraverso gli attribute che trovo sul sito rcsb search api)
-q1 = TextQuery('"sars-cov-2"')
-q2 = TextQuery('"antibody"') 
-q3 = TextQuery('"spike|receptor binding domain"')
-q4 = attrs.rcsb_accession_info.initial_release_date >= "2021-06-15T00:00:00Z"
-# q5 = TextQuery('"variant"')
-# q5 = attrs.entity_src_gen.pdbx_host_org_scientific_name != "homo sapiens"
+	q1 = TextQuery('"sars-cov-2"')
+	q2 = TextQuery('"antibody"')
+	q3 = TextQuery('"spike|receptor binding domain"')
+	q4 = attrs.rcsb_accession_info.initial_release_date >= "2021-07-27T00:00:00Z"
+	# q5 = TextQuery('"variant"')
 
-query = q1 & q2 & q3 & q4 #& q5
-# combined using bitwise operators (&, |, ~, etc)
-for pdb_code in query("entry"):
-    print(pdb_code)
-# #Per controllare i codici PDB trovati
+	query = q1 & q2 & q3 & q4 #& q5
+	# # combined using bitwise operators (&, |, ~, etc)
 
-# PDB = []
-# PDB.append(results)
-# PDB_str = []
-# PDB_str = ''.join(PDB)
-
-# print(PDB_str)
-
-
-# with open('weeklyresults.txt', 'w') as f:
-#    f.write(results)
-
-
-# import webbrowser
-# key = PDB_str   
-# check = ("https://rcsb.org/structure/" + key)
-# webbrowser.open(check)
-# print(key)
-       
+#	for pdb_code in query("entry"):
+#    		print(pdb_code)
+	for pdb_code in query("entry"):
+		pdb_code = pdb_code.split('\n')
+        	# print(pdb_code)
+		for l in pdb_code:
+			g = l,''
+            		#divido ogni codice PDB
+			g = ' '.join(g)
+           	 	#rendo ogni codice PDB una stringa
+			f.write(g)
+			print(g)
 # #dei PDB trovati mi serve capire se fanno a caso nostro, li cerco su internet e controllo
-# import webbrowser
- # key = PDB.append()
- #    check = ("https://rcsb.org/structure/" + key)
- #    webbrowser.open(check)
+			check = ("https://rcsb.org/structure/" + g)
+			webbrowser.open(check)
