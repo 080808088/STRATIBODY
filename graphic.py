a10811b1b6009f8b819af4da57c5410ba93018d4
14802292 <beatrice.tropea@gmail.com>
2021-11-25 10:13:31 +0100

    update

--- graphic.py
+++ graphic.py
@@ -10,17 +10,16 @@ import os, sys
 from io import BytesIO
 
 def make_graphic(f):
-	a = pd.read_csv(f)
-	x = [int(b) for b in list(range(101))]
+        a = pd.read_csv(f)
+        x = [int(b) for b in list(range(101))]
         y = a.iloc[:, 2]
 
-	fig, ax = plt.subplots(1, 1)  # Create a figure and an axes.
-	ax.plot(x, y)  # Plot some data on the axes.
-	ax.set_xlabel('ns')  # Add an x-label to the axes.
-	ax.set_ylabel('affinity_score')  # Add a y-label to the axes.
-	ax.set_title("Affinity Plot" + "_" + str(sys.argv[3]) + "-" + str(sys.argv[4]))  # Add a title to the axes.
+        fig, ax = plt.subplots(1, 1)  # Create a figure and an axes.
+        ax.plot(x, y)  # Plot some data on the axes.
+        ax.set_xlabel('ns')  # Add an x-label to the axes.
+        ax.set_ylabel('affinity_score')  # Add a y-label to the axes.
+        ax.set_title("Affinity Plot" + "_" + str(sys.argv[3]) + "-" + str(sys.argv[4]))  # Add a title to the axes.
         plt.ylim(0,1)   # Normalize value y.
-
-	out = plt.savefig("grafico.png", format = 'png', dpi = 600)
-	out = plt.close()
-	return(out)
+        out = plt.savefig("grafico.png", format = 'png', dpi = 600)
+        out = plt.close()
+        return(out)
